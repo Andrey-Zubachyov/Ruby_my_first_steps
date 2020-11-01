85a0d1a62f65de6ce2132fb66bfdd0afc01bdff4
Andrey-Zubachyov <53438644+Andrey-Zubachyov@users.noreply.github.com>
2020-11-01 23:26:47 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -2,3 +2,4 @@ puts "I would hug you, but I’m just a text"
 #Теперь мы выводим на экран строку и вместо того, чтобы выходить из программы - ожидаем ввода.
 puts "I would hug you, but I’m just a text"
 gets
+#“gets” это по сути “get string” - получить строку.
