995f5043b439a64ca342039ac75126df2ff5e580
Andrey-Zubachyov <53438644+Andrey-Zubachyov@users.noreply.github.com>
2020-11-01 23:57:11 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
+#"#" - hash, иногда говорят pound sign
+# поддержка русского языка, а точнее правильной кодировки - encoding: cp866 в ОС Windows
+#1.
+#Выводим строку на экран
 puts "I would hug you, but I’m just a text"
+#2.
 #Теперь мы выводим на экран строку и вместо того, чтобы выходить из программы - ожидаем ввода.
 puts "I would hug you, but I’m just a text"
 gets
 #“gets” это по сути “get string” - получить строку.
+#3.
+#составим простейшую программу для изучения иностранного языка. Возьмем три слова: ball, door, peace.
+puts "How to translate ball?"
+gets
+puts "How to translate door?"
+gets
+puts "How to translate peace?"
+gets
