48b03de3f15fb02581669f6ee5203e795eb41cbd
Andrey-Zubachyov <53438644+Andrey-Zubachyov@users.noreply.github.com>
2020-11-01 23:25:43 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,1 +1,4 @@
 puts "I would hug you, but I’m just a text"
+#Теперь мы выводим на экран строку и вместо того, чтобы выходить из программы - ожидаем ввода.
+puts "I would hug you, but I’m just a text"
+gets
