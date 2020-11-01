88fb2acb90dad409a267acd1145474b5b8f89496
Andrey-Zubachyov <53438644+Andrey-Zubachyov@users.noreply.github.com>
2020-11-02 00:04:25 +0300

    Update add_1.rb

--- add_1.rb
+++ add_1.rb
@@ -8,3 +8,20 @@ puts "Your age?"
 age = gets
 puts "Your age is"
 puts age
+=begin
+Существуют т.н. naming conventions - соглашения о наименовании - их
+достаточно просто найти: ввести в поисковой системе запрос “naming conventions
+variables ruby”.
+В языках программирования Ruby и JavaScript мы столкнемся с тремя основными naming
+conventions:
+● Snake case (snake - змея), между словами ставится знак подчеркивания underscore
+(`_`). Переменные именуются следующим образом: client_age, user_password,
+user_password_expiration_date. Используется в руби, а также в базах данных.
+● Camel case (camel - верблюд), слово начинается с маленькой буквы, слова
+разделяются с помощью больших букв: clientAge, userPassword,
+userPasswordExpirationDate. Используется в JavaScript.
+● Kebab case (kebab - шашлык), слова разделяются дефисом: client-age, userpassword,
+user-password-expiration-date. Иногда используется в HTML, в т.н. data-
+атрибутах. Например: `<input type="text" name="login" data-error-highlight-input type="text" name="login" data-error-highlightcolor="
+red">`.
+=end
