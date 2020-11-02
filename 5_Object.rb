7cbdea47b7c0c5050586cd679e904b83a5dad782
Andrey-Zubachyov <53438644+Andrey-Zubachyov@users.noreply.github.com>
2020-11-02 19:12:02 +0300

    Update 5_Object.rb

--- 5_Object.rb
+++ 5_Object.rb
@@ -1,1 +1,10 @@
+# Все в руби - объект
+# Everything in Ruby - Object
 
+#Известно, что `123.class` возвращает Integer, `"blabla".class` возвращает String. Но у
+#объекта (Object) существует также метод `is_a?`, который возвращает истину или ложь,
+#если передать определенный параметр в этот метод:
+
+p 123.is_a?(Integer) #=> true
+p 123.is_a?(Integer) #=> false
+p 123.is_a?(Object) #=> true
