9f673a8a9b97932a4b4b0ffe88e3bc13fce5b968
Andrey-Zubachyov <53438644+Andrey-Zubachyov@users.noreply.github.com>
2020-11-02 19:15:25 +0300

    Update 6_type_casting.rb

--- 6_type_casting.rb
+++ 6_type_casting.rb
@@ -1,1 +1,16 @@
+#Приведение типов
+#converting types или type casting
 
+#программа, которая считает, сколько вам месяцев
+
+puts "Your age?"
+age = gets #“get string”
+age_months = age.to_i * 12
+puts "Your age is " + age_months.to_s
+
+#можно вообще обойтись без переменной age_months. Попробуйте написать такую программу самостоятельно.
+puts "Your age?"
+age = gets.to_i * 12
+puts "Your age is " + age.to_s
+
+# методы .to_s - преобразуем в строку .to_i - преобразуем в число .to_f - преобразуем в числос плавающей запятой 
