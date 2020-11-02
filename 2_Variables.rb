edadcb6a4be4547b85a01cdbab8ca559976fda50
Andrey-Zubachyov <53438644+Andrey-Zubachyov@users.noreply.github.com>
2020-11-02 19:05:41 +0300

    Update 2_Variables.rb

--- 2_Variables.rb
+++ 2_Variables.rb
@@ -25,3 +25,29 @@ user-password-expiration-date. Иногда используется в HTML, в
 атрибутах. Например: `<input type="text" name="login" data-error-highlight-input type="text" name="login" data-error-highlightcolor="
 red">`.
 =end
+
+#Задание:
+=begin
+написать программу, которая подряд спрашивает год рождения, место
+рождения, номер телефона трех клиентов, после чего выводит полученную информацию
+полностью в виде “карточек” (в англ.языке это бы называлось baseball card, аналогия в
+русском языке - карточка из картотеки).
+=end
+puts "Your age?"
+age_1 = gets
+puts "Your place of birth?"
+birth_1 = gets 
+puts "Your number phone?"
+phone_1 = gets 
+puts "Your age?"
+age_2 = gets
+puts "Your place of birth?"
+birth_2 = gets 
+puts "Your number phone?"
+phone_2 = gets
+puts "Your age?"
+age_3 = gets
+puts "Your place of birth?"
+birth_3 = gets 
+puts "Your number phone?"
+phone_3 = gets 
