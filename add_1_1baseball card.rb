e947c59fdf515788cefa99ae79def5c5e2c85735
Andrey-Zubachyov <53438644+Andrey-Zubachyov@users.noreply.github.com>
2020-11-02 07:07:17 +0300

    Update add_1_1baseball card.rb

--- add_1_1baseball card.rb
+++ add_1_1baseball card.rb
@@ -4,3 +4,15 @@ puts "Your place of birth?"
 birth = gets 
 puts "Your number phone?"
 phone = gets 
+puts "Your age?"
+age = gets
+puts "Your place of birth?"
+birth = gets 
+puts "Your number phone?"
+phone = gets
+puts "Your age?"
+age = gets
+puts "Your place of birth?"
+birth = gets 
+puts "Your number phone?"
+phone = gets 
